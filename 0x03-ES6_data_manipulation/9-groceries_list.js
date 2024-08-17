7a640c2bd96607dcac2b889f904f7b5fb2180aa3
Evelynewaters <eveclaraify@gmail.com>
2024-08-17 14:27:55 +0000

    Docs- new solutions added

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,14 +2,11 @@
  * Retrieves a map of grocery names and their quantity.
  * @returns {Map<string, Number>}
  */
-export default function groceriesList() {
-  const values = [
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ];
-  return new Map(values);
-}
+const groceriesList = () => new Map()
+  .set('Apples', 10)
+  .set('Tomatoes', 10)
+  .set('Pasta', 1)
+  .set('Rice', 1)
+  .set('Banana', 5);
 
+export default groceriesList;
